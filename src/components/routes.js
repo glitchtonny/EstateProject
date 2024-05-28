6c750d02bd81e3625e8e2beea84b3ae4aea580f1
j0yglvdy5 <joygladys.njeri@student.moringaschool.com>
2024-05-28 11:27:03 +0300

    routes

---
+++ src/components/routes.js
@@ -0,0 +1,15 @@
+import Home from "./Home";
+import Blog from "./Blog";
+
+const routes =[
+    {
+        path:"/Home",
+        element:<Home/>
+    },
+    {
+        path:"/Blog",
+        element:<Blog/>
+    }
+];
+
+export default routes;
